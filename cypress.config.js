0af79f6f1680199524e34f1a99e6813814536a4a
LucasSantos <lucas.fariasbsi@hotmail.com>
2023-02-08 22:04:16 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
     e2e: {
         baseUrl: 'https://api.openweathermap.org/data/2.5',
-        API_key: '${{vars.API_KEY}}'
+ 
     },
     fixturesFolder: false,
     video: false,
